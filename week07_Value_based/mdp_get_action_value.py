6ad698b0d3f0353763284aa5d0016587f37e2e70
Peter <mokrov.pv@phystech.edu>
2019-11-24 01:53:16 +0300

    Value based Seminar completed (base part)

--- week07_Value_based/mdp_get_action_value.py
+++ week07_Value_based/mdp_get_action_value.py
@@ -1,9 +1,14 @@
+
 def get_action_value(mdp, state_values, state, action, gamma):
     """ Computes Q(s,a) as in formula above """
-    
-    accumulator = 0.
-    # YOUR CODE HERE
-    for next_state, prob in mdp.get_next_states(state, action).items():
-        transition_reward = mdp.get_reward(state, action, next_state)
-        accumulator += prob * (transition_reward + gamma * state_values[next_state])
-    return accumulator
+    # s = `state` 
+    # a = `action`
+    next_states = mdp.get_next_states(state, action) # set of s'
+    q_s_a = 0
+    for s_tch in next_states:
+        p = mdp.get_transition_prob(state, action, s_tch)
+        r = mdp.get_reward(state, action, s_tch)
+        v = state_values[s_tch]
+        q_s_a += p * (r + gamma*v)
+
+    return q_s_a
